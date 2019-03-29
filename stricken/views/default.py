2134856f732c0c02880b937c2100a8b18e98dce9
Bowe Strickland <bowe@redhat.com>
2019-03-29 10:19:48 -0400

    visit count

--- stricken/views/default.py
+++ stricken/views/default.py
@@ -11,6 +11,8 @@ def my_view(request):
     try:
         query = request.dbsession.query(MyModel)
         one = query.filter(MyModel.name == 'one').first()
+        one.value += 1
+        request.dbsession.add(one)
     except DBAPIError:
         return Response(db_err_msg, content_type='text/plain', status=500)
     return {'one': one, 'project': 'stricken'}
